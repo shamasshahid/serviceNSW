5cebdd0049c2cab5e24ce161f89fe3ec52fe76cc
Shamas <shamas.shahid@gmail.com>
2020-09-28 23:05:04 +1000

    added initial xcode

---
+++ RegoApp/RegoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RegoApp
+//
+//  Created by Shamas on 28/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
